775fec4ef6aba237b03c0748cc74f7b0e8cc42bf
mtai0524 <115647147+mtai0524@users.noreply.github.com>
2023-09-18 13:19:46 +0700

    QR code 2FA

--- CodeFirst/CodeFirst/Areas/Identity/Pages/Account/LoginWithRecoveryCode.cshtml.cs
+++ CodeFirst/CodeFirst/Areas/Identity/Pages/Account/LoginWithRecoveryCode.cshtml.cs
@@ -5,6 +5,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
+using CodeFirst.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -14,13 +15,13 @@ namespace CodeFirst.Areas.Identity.Pages.Account
 {
     public class LoginWithRecoveryCodeModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger<LoginWithRecoveryCodeModel> _logger;
 
         public LoginWithRecoveryCodeModel(
-            SignInManager<IdentityUser> signInManager,
-            UserManager<IdentityUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            UserManager<ApplicationUser> userManager,
             ILogger<LoginWithRecoveryCodeModel> logger)
         {
             _signInManager = signInManager;
