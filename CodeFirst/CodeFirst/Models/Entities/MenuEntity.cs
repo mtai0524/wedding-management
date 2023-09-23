4ef4f659f0dbaad5d3096a651ae3d87d55221360
mtai0524 <115647147+mtai0524@users.noreply.github.com>
2023-09-23 08:47:46 +0700

    validate menu

--- CodeFirst/CodeFirst/Models/Entities/MenuEntity.cs
+++ CodeFirst/CodeFirst/Models/Entities/MenuEntity.cs
@@ -7,15 +7,18 @@ namespace CodeFirst.Models.Entities
     {
         [Key]
         public int MenuId { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập tên món ăn")]
         [Display(Name ="Tên món ăn")]
         public string? Name { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập giá món ăn")]
         [Display(Name = "Giá món ăn")]
         public double? Price { get; set; }
         [Display(Name = "Mô tả món ăn")]
         public string? Description { get; set; }
-        public int CategoryId { get; set; } // nhớ get set dùm
         [Display(Name = "Thể loại")]
+        public int CategoryId { get; set; } // nhớ get set dùm
         [ForeignKey("CategoryId")]
+        [Display(Name = "Thể loại")]
         public virtual MenuCategory? MenuCategory { get; set; }
     }
 }
