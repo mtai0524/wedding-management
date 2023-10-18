d33ca479d05d5f4029c973695bd27f41d05a0de1
mtai0524 <115647147+mtai0524@users.noreply.github.com>
2023-10-18 12:27:01 +0700

    update UI

---
+++ CodeFirst/CodeFirst/Models/Entities/FeedbackModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeFirst.Models.Entities
+{
+    public class FeedbackModel
+    {
+        public string UserId { get; set; }
+        public string Content { get; set; }
+        public double Rating { get; set; }
+        public int BranchId { get; set; }
+    }
+}
--- CodeFirst/WebAPI/Controllers/ApiFeedbackController.cs
+++ CodeFirst/WebAPI/Controllers/ApiFeedbackController.cs
@@ -1,4 +1,5 @@
 ﻿using CodeFirst.Data;
+using CodeFirst.Models.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -21,6 +22,7 @@ namespace WebAPI.Controllers
             var feedbacks = _context.Feedback
                         .Where(f => f.BranchId == branchId)
                         .Include(f => f.Id) // Nạp thông tin người dùng
+                        .OrderByDescending(f => f.FeedbackDate) // Sắp xếp giảm dần theo FeedbackDate
                         .ToList();
 
             if (feedbacks == null)
@@ -29,6 +31,37 @@ namespace WebAPI.Controllers
             }
 
             return Ok(feedbacks);
+        }
+        [HttpPost]
+        public IActionResult CreateFeedback([FromBody] FeedbackModel feedbackModel)
+        {
+            try
+            {
+                if (feedbackModel == null)
+                {
+                    return BadRequest("Dữ liệu phản hồi không hợp lệ");
+                }
+
+                // Tạo một đối tượng Feedback từ dữ liệu đầu vào
+                var feedback = new Feedback
+                {
+                    UserId = feedbackModel.UserId,
+                    FeedbackDate = DateTime.Now, // Đặt ngày phản hồi thành ngày hiện tại
+                    Content = feedbackModel.Content,
+                    Rating = feedbackModel.Rating,
+                    BranchId = feedbackModel.BranchId
+                };
+
+                // Thêm đối tượng phản hồi mới vào cơ sở dữ liệu
+                _context.Feedback.Add(feedback);
+                _context.SaveChanges();
+
+                return Ok("Phản hồi đã được tạo thành công");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
     }
 }
