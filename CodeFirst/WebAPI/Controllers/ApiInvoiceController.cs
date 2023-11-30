5699ff3f368beb35e113ee61eea40edf38c3bf2f
mtai0524 <115647147+mtai0524@users.noreply.github.com>
2023-11-30 18:54:58 +0700

    mailtrap.io n smtp

--- CodeFirst/WebAPI/Controllers/ApiInvoiceController.cs
+++ CodeFirst/WebAPI/Controllers/ApiInvoiceController.cs
@@ -4,6 +4,10 @@ using CodeFirst.Models.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json.Linq;
 
 namespace WebAPI.Controllers
 {
@@ -60,6 +64,9 @@ namespace WebAPI.Controllers
                 Note = request.Note,
             };
 
+
+
+
             // Kiểm tra ngày AttendanceDate
             if (request.AttendanceDate.HasValue)
             {
@@ -83,6 +90,7 @@ namespace WebAPI.Controllers
                 MenuId = orderMenu.MenuID
             }).ToList();
 
+
             // Thêm danh sách các món đã đặt vào DbContext và lưu vào cơ sở dữ liệu
             _context.OrderMenu.AddRange(orderMenus);
             await _context.SaveChangesAsync();
@@ -96,6 +104,8 @@ namespace WebAPI.Controllers
             // Thêm danh sách các món đã đặt vào DbContext và lưu vào cơ sở dữ liệu
             _context.OrderService.AddRange(orderServices);
             await _context.SaveChangesAsync();
+            SendMail(request, invoice, orderMenus, orderServices);
+
 
             //if (request.InvoiceCode != null && request.InvoiceCode.Any())
             //{
@@ -133,6 +143,119 @@ namespace WebAPI.Controllers
                 .ToList();
 
             return Ok(bookedHalls);
+        }
+     
+        void SendMail(InvoiceAndOrderMenusRequest request, Invoice invoice, List<OrderMenu> orderMenus, List<OrderService> orderServices)
+        {
+            StringBuilder body = new StringBuilder();
+            body.AppendLine("<html><head>");
+            body.AppendLine("<style>");
+            body.AppendLine("body { font-family: Arial, sans-serif; margin: 0; padding: 0; }");
+            body.AppendLine(".container { width: 80%; margin: auto; overflow: hidden; }");
+            body.AppendLine("header { background: #103151; color: white; padding-top: 30px; min-height: 70px; border-bottom: #D4BB72 4px solid; }");
+            body.AppendLine("header a { color: white; text-decoration: none; text-transform: uppercase; font-size: 16px; }");
+            body.AppendLine("header ul { padding: 0; margin: 0; float: right; margin-top: 20px; list-style: none; }");
+            body.AppendLine("header #branding { float: left; margin: 0; padding: 0; }");
+            body.AppendLine("header #branding img { height: 80px; }");
+            body.AppendLine("header .highlight, header .current a { color: #E8BD72; font-weight: bold; }");
+            body.AppendLine("header a:hover { color: #ffffff; font-weight: bold; }");
+            body.AppendLine(".main { padding: 0; }");
+            body.AppendLine(".main h2 { color: #333; }");
+            body.AppendLine(".main p { font-size: 18px; color: #666; }");
+            body.AppendLine("table { border-collapse: collapse; width: 100%; }");
+            body.AppendLine("th, td { border: 1px solid #dddddd; text-align: left; padding: 12px; font-size: 16px; }");
+            body.AppendLine("th { background-color: #f2f2f2; }");
+            body.AppendLine("</style>");
+            body.AppendLine("</head><body>");
+            body.AppendLine("<header>");
+            body.AppendLine("<div class=\"container\">");
+            body.AppendLine("<div id=\"branding\">");
+            // Thêm ảnh logo vào giữa header
+            body.AppendLine("<img src=\"https://royalhelmet.com.vn/ckfinder/userfiles/images/logo.png\" alt=\"Logo\" />");
+            body.AppendLine("<h1><span class=\"highlight\">Royal Helmet</span></h1>");
+            body.AppendLine("</div>");
+            body.AppendLine("</div>");
+            body.AppendLine("</header>");
+            body.AppendLine("<div class=\"container main\">");
+            body.AppendLine("<h2>Chi tiết đơn hàng:</h2>");
+
+            body.AppendLine($"<p><strong>Mã đơn hàng:</strong> {invoice.InvoiceID.ToString()}</p>");
+            var customer = _context.ApplicationUser.FirstOrDefault(x => x.Id == request.UserId);
+
+            body.AppendLine($"<p><strong>Email đặt hàng:</strong> {customer.Email}</p>");
+            body.AppendLine($"<p><strong>Số điện thoại:</strong> {customer.PhoneNumber}</p>");
+
+            body.AppendLine($"<p><strong>Ngày đặt hàng:</strong> {invoice.InvoiceDate}</p>");
+            body.AppendLine($"<p><strong>Sản phẩm:</strong></p>");
+
+
+            body.AppendLine("<table>");
+            body.AppendLine("<tr><th>Hình ảnh</th><th>Tên sản phẩm</th><th>Giá bán</th></tr>");
+
+            // danh sách món ăn đặt nhà hàng
+            foreach (var orderMenu in orderMenus)
+            {
+                var item = _context.MenuEntity.Find(orderMenu.MenuId);
+                body.AppendLine($"<tr>");
+                body.AppendLine($"<td style='width: 150px'><img src='{item.Image}' alt='Logo' style='width: 100%; height: 120px;' /></td>");
+                body.AppendLine($"<td>{item.Name}</td>");
+                body.AppendLine($"<td>{item.Price.Value.ToString("#,##0")} VND</td>");
+                body.AppendLine($"</tr>");
+            }
+            // danh sách dịch vụ đặt nhà hàng
+            foreach (var orderService in orderServices)
+            {
+                var item = _context.ServiceEntity.Find(orderService.ServiceId);
+                body.AppendLine($"<tr>");
+                body.AppendLine($"<td style='width: 150px'><img src='{item.Image}' alt='Logo' style='width: 100%; height: 120px;' /></td>");
+                body.AppendLine($"<td>{item.Name}</td>");
+                body.AppendLine($"<td>{item.Price.Value.ToString("#,##0")} VND</td>");
+                body.AppendLine($"</tr>");
+            }
+            body.AppendLine($"<tr><td colspan='2'><strong>Tổng tiền sau khi áp dụng mã giảm:</strong></td><td>{invoice.Total.Value.ToString("#,##0")} VND</td></tr>");
+
+
+            body.AppendLine("</table>");
+            body.AppendLine("</div>");
+
+            body.AppendLine("<footer style=\"margin-top:20px;background: #262626; color: white; padding: 20px 0;\">");
+            body.AppendLine("<div class=\"container\">");
+            body.AppendLine("<p>&copy; 2023 Royal Helmet. </p>");
+            body.AppendLine("</div>");
+            body.AppendLine("</footer>");
+
+            body.AppendLine("</body></html>");
+
+            // Gửi email
+            MailMessage mail = new MailMessage();
+            //mail.To.Add(customer.Email.ToString());
+            mail.To.Add("minhnguyen20020524@gmail.com"); // email người nhận 
+
+            mail.From = new MailAddress("duatreodaiduongden@gmail.com");// email người gửi 
+            mail.Subject = "ROYAL HELMET - CHI TIẾT ĐƠN HÀNG";
+            mail.Body = body.ToString();
+            mail.IsBodyHtml = true; // Bật chế độ HTML
+
+            //SmtpClient smtp = new SmtpClient("sandbox.smtp.mailtrap.io");
+            //smtp.EnableSsl = true;
+            //smtp.Port = 2525;
+            //smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            //smtp.Credentials = new NetworkCredential("f53ec0c5d129dd", "647d8437d3d40c");
+
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com"); // máy chủ smtp của google
+            smtp.EnableSsl = true;
+            smtp.Port = 587; // port client mặc định hầu như máy nào cũng vậy
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.Credentials = new NetworkCredential("duatreodaiduongden@gmail.com", "aiyt kzuj xpbq ygda"); // từ năm 2022 trở đi dùng mật khẩu do gmail cấp 
+
+            try
+            {
+                smtp.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                // Xử lý nếu có lỗi khi gửi mail
+            }
         }
 
         [HttpGet("get-invoice/{userId}")]
