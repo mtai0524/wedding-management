582e0ff8cda6985566623a947b75a4a656f6c96d
mtai0524 <115647147+mtai0524@users.noreply.github.com>
2024-03-01 18:32:36 +0700

    feat: add tailwind

--- CodeFirst/CodeFirst/obj/Debug/net6.0/CodeFirst.AssemblyInfo.cs
+++ CodeFirst/CodeFirst/obj/Debug/net6.0/CodeFirst.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeFirst")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+754235041bbf6f09b7bbfd91602042d715d194c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94d6a9c23311aaa955014b832ebb101a09434937")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeFirst")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeFirst")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ CodeFirst/CodeFirst/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+       './Pages/**/*.cshtml',
+       './Views/**/*.cshtml',
+       './Areas/Admin/Views/**/*.cshtml'
+],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
