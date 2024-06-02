78d584996070b01d0dea0a177019e12038cece2d
mtai0524 <115647147+mtai0524@users.noreply.github.com>
2024-06-02 11:35:27 +0700

    fix: task to do

--- CodeFirst/MyTesting/obj/Debug/net6.0/MyTesting.AssemblyInfo.cs
+++ CodeFirst/MyTesting/obj/Debug/net6.0/MyTesting.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyTesting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ede763614426c26570e22039f2e8f5564ccd73da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e54dd5f1629eb58116cd37c789e60d6f39c1dd00")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyTesting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyTesting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
