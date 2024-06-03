97b810b8318cc87dc6a5ddfa7cdb10f132569214
mtai0524 <115647147+mtai0524@users.noreply.github.com>
2024-06-03 22:04:53 +0700

    feat: send mail when delete invoice

--- CodeFirst/MyTesting/obj/Debug/net6.0/MyTesting.AssemblyInfo.cs
+++ CodeFirst/MyTesting/obj/Debug/net6.0/MyTesting.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyTesting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e83c07636ec965099501c2777188cd702f70c26a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad8d003c29e0adbf0616ebfebadc633bda5419f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyTesting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyTesting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CodeFirst/WebAPI/obj/Debug/net6.0/WebAPI.AssemblyInfo.cs
+++ CodeFirst/WebAPI/obj/Debug/net6.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e83c07636ec965099501c2777188cd702f70c26a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad8d003c29e0adbf0616ebfebadc633bda5419f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
