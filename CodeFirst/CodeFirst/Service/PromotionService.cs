beb7dc5dc094b933d504eb761c7dbe26db93c20d
mtai0524 <115647147+mtai0524@users.noreply.github.com>
2024-07-06 13:51:02 +0700

    feat: promotion blazor

---
+++ CodeFirst/CodeFirst/Service/PromotionService.cs
@@ -0,0 +1,20 @@
+﻿using CodeFirst.Data;
+using CodeFirst.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirst.Service
+{
+    public class PromotionService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PromotionService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Code>> GetPromotionsAsync()
+        {
+            return await _context.Code.ToListAsync();
+        }
+    }
+}
---
+++ CodeFirst/CodeFirst/wwwroot/js/blazorjs.js
@@ -0,0 +1,11 @@
+﻿window.blazorjs = {
+    generateRandomCode: function () {
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        let code = '';
+        for (let i = 0; i < 15; i++) {
+            const randomIndex = Math.floor(Math.random() * characters.length);
+            code += characters.charAt(randomIndex);
+        }
+        return code;
+    }
+};
