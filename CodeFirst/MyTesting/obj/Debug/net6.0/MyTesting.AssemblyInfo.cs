2ae7c6884628f92149f27df972031ecc574eefcb
mtai0524 <115647147+mtai0524@users.noreply.github.com>
2024-07-09 08:53:50 +0700

    chors: change api gateway pay invoice

--- CodeFirst/MyTesting/obj/Debug/net6.0/MyTesting.AssemblyInfo.cs
+++ CodeFirst/MyTesting/obj/Debug/net6.0/MyTesting.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyTesting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+158fdf39685bf695510102a56ed6c35051bdc5e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50eb681078fe0b6a5af0106a2fda21d3a22766ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyTesting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyTesting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
